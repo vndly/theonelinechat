7940b61260bc2ac2eb3abe3e260c9ca5d96ec2c8
Alban <val1964@gmail.com>
2025-03-02 16:26:34 +0100

    Enter now cleans the input + random text color

--- public/logic.js
+++ public/logic.js
@@ -1,8 +1,44 @@
 import { listenChat, updateChat } from './firebase.js'
 
+// Generate or retrieve UID
+let uid = localStorage.getItem('uid')
+if (!uid) {
+  uid = generateUID()
+  localStorage.setItem('uid', uid)
+}
+
+function generateUID() {
+  return Math.random().toString(36).substr(2, 9)
+}
+
+let color = localStorage.getItem('color')
+if (!color) {
+  color = generateRandomColorWithoutBlue()
+  localStorage.setItem('color', color)
+}
+
+function generateRandomColorWithoutBlue() {
+  let color
+  do {
+    color = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`
+  } while (color.includes('00') || color.includes('ff'))
+  return color
+}
+
+console.log('uid:', uid)
+console.log('color:', color)
+
 const inputElement = document.getElementById('input')
-inputElement.addEventListener('keyup', function (event) {
-  updateChat(inputElement.value)
+
+inputElement.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    event.preventDefault()
+    updateChat('')
+  } else {
+    inputElement.addEventListener('keyup', function (event) {
+      updateChat(inputElement.value)
+    })
+  }
 })
 listenChat(updateInput)
 
@@ -19,6 +55,7 @@ inputElement.addEventListener('input', event => {
 
 function updateInput(text) {
   inputElement.value = text
+  inputElement.style.color = color
   updateHeight(inputElement)
 }
 
